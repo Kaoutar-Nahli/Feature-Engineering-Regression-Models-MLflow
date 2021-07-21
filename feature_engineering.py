e5dddeec94e0106c6ba4eaac1d985c19f7bc43d1
unknown <kautarnahli@hotmail.es>
2021-07-21 16:26:36 +0100

    cleaniing number of reviews

---
+++ feature_engineering.py
@@ -0,0 +1,23 @@
+#%%
+import pandas as pd
+#%%
+df = pd.read_csv('amazon_data_cleaned_01.csv')
+df.columns
+# %%
+df.drop('Unnamed: 0', axis=1,inplace=True)
+# %%
+
+# 'mat_url', 'name', 'number_reviews', 'rating',
+#        'reviews_text', 'table_features_color_care_material', 'other_prices',
+#        'other_colors', 'combined_price', 'weight', 'dimentions', 'care',
+#        'thickness', 'material', 'brand', 'color'
+pd.set_option('display.max_colwidth', None)
+df['number_reviews'] = df['number_reviews'].str.replace('ratings','')
+df['number_reviews'] = df['number_reviews'].str.replace('Nan','0')
+df['number_reviews'] = df['number_reviews'].str.replace(',','')
+df['number_reviews'] = df['number_reviews'].str.replace('rating','').astype('int')
+
+# %%
+df['number_reviews'].isna().sum()
+#df['number_reviews'].unique()
+# %%
