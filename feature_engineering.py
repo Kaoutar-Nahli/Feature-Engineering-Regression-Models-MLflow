d0eacb205a14be27d65bb5cf55640d0646e2e77a
unknown <kautarnahli@hotmail.es>
2021-07-21 23:01:44 +0100

    cleaning ratings

--- feature_engineering.py
+++ feature_engineering.py
@@ -11,13 +11,25 @@ df.drop('Unnamed: 0', axis=1,inplace=True)
 #        'reviews_text', 'table_features_color_care_material', 'other_prices',
 #        'other_colors', 'combined_price', 'weight', 'dimentions', 'care',
 #        'thickness', 'material', 'brand', 'color'
+#Cleaning number of reviews
 pd.set_option('display.max_colwidth', None)
 df['number_reviews'] = df['number_reviews'].str.replace('ratings','')
 df['number_reviews'] = df['number_reviews'].str.replace('Nan','0')
 df['number_reviews'] = df['number_reviews'].str.replace(',','')
 df['number_reviews'] = df['number_reviews'].str.replace('rating','').astype('int')
+df['other_prices'].unique()
+# %%
+#cleaning rating out of 5
+df['rating'] = df['rating'].str.replace('out of 5','').astype('float')
+#%%
+#cleaning other prices
+df['other_prices'] = df['other_prices'].str.replace('">','').str.replace('-',',').str.replace('$','')
+#astype('float')
+#%%
+print(df['other_prices'].isnull().sum(), df['other_prices'].isna().sum())
+df.loc[60,'other_prices']
+df['other_prices'].value_counts()
 
 # %%
-df['number_reviews'].isna().sum()
-#df['number_reviews'].unique()
+df.shape
 # %%
