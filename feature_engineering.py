98fa0bf173a63a3c1011eebf871466476e557b9b
unknown <kautarnahli@hotmail.es>
2021-07-22 13:58:19 +0100

    separating LxWxT

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,9 +1,14 @@
 #%%
 import pandas as pd
 import numpy as np
+import re
 #%%
 df = pd.read_csv('amazon_data_cleaned_01.csv')
 df.columns
+df = df.replace('nan', np.nan, regex=True)
+df = df.replace(0, np.nan, regex=True)
+
+  
 # %%
 df.drop('Unnamed: 0', axis=1,inplace=True)
 # %%
@@ -59,18 +64,63 @@ class dimention_convertion():
            self.value =  self.value / 1000
 #apply convertion_dimention class
 df['weight_kg'] = df.apply(lambda row: dimention_convertion(row.weight_float ,row.weight_dimention).convert(), axis=1)
-df.drop(['weight_float','weight_dimention','weight','mat_url', 'name','table_features_color_care_material'], axis=1, inplace=True)
+df.drop(['weight_float','weight_dimention','weight','mat_url','table_features_color_care_material'], axis=1, inplace=True)
 # %%
 df.head()
 #%%
 df.shape
+
 # %%
-#Converting dimention in volume
-df[]
+#Converting dimentions and thickness in three rows: L X W X thickness_two
+#L
+df['dimentions'] = df['dimentions'].str.strip().str.lower()
+df['L'] = df['dimentions'].apply(lambda x:  x if type(x)==float else x.split('x',1)[0]).astype(float)
+
+#W
+df['W'] = df['dimentions'].apply(lambda x:  x if type(x)==float else x.split('x',1)[1]).str.strip()
+df['W'] = df['W'].apply(lambda x:  x if type(x)==float else x.split(' ',1)[0]).astype(float)
+
+#thickness
+#select from W forward
+df['thickness_2'] = df['dimentions'].apply(lambda x:  x if type(x)==float else x.split('x',1)[1]).str.strip()
+#select from thickness forward
+df['thickness_2'] = df['thickness_2'].apply(lambda x:  x if type(x)==float else x.split(' ',1)[1]).str.strip()
+
+#select only thickness
+regex_thickness = re.compile(r'[\d+\.]+[\d*]')
+df['thickness_2']= df['thickness_2'].apply(lambda x:  x if type(x)==float else re.findall(regex_thickness,x))
+df['thickness_2'] = df['thickness_2'].apply(lambda y: np.nan  if y==[] else float(str(y).replace("['",'').replace("']",'')))
+df['thickness_2'][df['thickness_2'].notnull()]
+#dimention
+#select only text
+regex_dimention = re.compile(r'[a-z]+')
+df['dimention_LXW']= df['dimentions'].apply(lambda x:  x if type(x)==float else re.findall(regex_dimention,x))
+df['dimention_LXW'] = df['dimention_LXW'].apply(lambda y: np.nan  if y==[]  else str(y))
+#df['dimention_LXW'].unique()
+#all is in inches 
+
+
 #%%
+df['thickness']
+#df.isna().sum()
+#df['rating'].unique()
 #  'number_reviews', 'rating',
 #        'reviews_text', 'other_prices',
 #        'other_colors', 'combined_price', 'weight',
 #  'dimentions', 'care',
 #   'thickness', 'material', 'brand', 'color'
+# %%
+
+regex_thickness = re.compile(r'([\d+\s*a*]+x[\s*\d+\s*])|([\d*\s*a*]+X[\s*\d*\s*])')
+df['test'] = df['name'].str.findall(regex_thickness)
+df['test']
+# %%
+for i in ['test']:
+    try:
+        df[i] = df[i].apply(lambda y:np.nan  if y==[] else y)
+    except Exception as e:
+        print(e)
+df['test'].isna().sum()
+# %%
+df['test'][df['test'].notnull()]
 # %%
